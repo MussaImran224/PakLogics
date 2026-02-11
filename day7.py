a4774f26146653171853a6a82d8a142cef64d356
MussaImran224 <geniusmussa@gmail.com>
2026-02-12 01:05:14 +0500

    Update day7.py

--- day7.py
+++ day7.py
@@ -1,1 +1,15 @@
-f=open("file.txt","w")
+#day 7
+#practise problem 1 read and count lines
+# with open('file.txt','r') as f:
+#     count=0    
+#     for x in f:
+#         print(x)  
+#         count=count+1
+# print(count)
+#practise problem 2 Count words in file
+with open('file.txt','r') as f:
+    count=0    
+    for x in f:
+        content=f.read()
+        count=len(content)
+print(count)            
